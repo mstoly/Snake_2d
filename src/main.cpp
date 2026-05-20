840696ca434e44392806fe0b4d7cde82c47e8fc6
Maksym Stolyarenko <m.stolyarenko@gmail.com>
2026-05-20 19:41:26 +0300

    Adjust window context

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,15 @@
 
 int main(void)
 {
-    GLFWwindow* window;
+    GLFWwindow *window;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(640, 480, "Hello World", nullptr, nullptr);
