843f3829ea3a43f4c130993acb991cae7445874e
Maksym Stolyarenko <m.stolyarenko@gmail.com>
2026-05-28 00:35:54 +0300

    Draw grid

--- src/Render.cpp
+++ src/Render.cpp
@@ -1,9 +1,88 @@
+#include <iostream>
+#include <glm/gtc/type_ptr.hpp>
+
 #include "Render.h"
 
 Render::Render()
 {
+    initGrid();
+    bindGrid();
+
+    gridShaderProgram = std::make_unique<Shader>("shaders/grid.vert", "shaders/grid.frag");
 }
 
 void Render::Draw()
 {
+    drawGrid();
+}
+
+void Render::bindGrid()
+{
+    GLuint VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    glBindVertexArray(VAO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+
+    glBufferData(
+        GL_ARRAY_BUFFER,
+        vertices.size() * sizeof(float),
+        vertices.data(),
+        GL_STATIC_DRAW);
+
+    glVertexAttribPointer(
+        0,
+        2,
+        GL_FLOAT,
+        GL_FALSE,
+        2 * sizeof(float),
+        (void *)0);
+
+    glEnableVertexAttribArray(0);
+}
+
+void Render::drawGrid()
+{
+    gridShaderProgram->Use();
+
+    glUniformMatrix4fv(
+        glGetUniformLocation(gridShaderProgram->GetProgramId(), "uProj"),
+        1,
+        GL_FALSE,
+        glm::value_ptr(proj));
+
+    glBindVertexArray(VAO);
+
+    glDrawArrays(
+        GL_LINES,
+        0,
+        vertices.size() / 2);
+}
+
+void Render::initGrid()
+{
+    for (int x = 0; x <= cols; ++x)
+    {
+        float px = x * cell;
+
+        vertices.push_back(px);
+        vertices.push_back(0.0f);
+
+        vertices.push_back(px);
+        vertices.push_back(height);
+    }
+
+    for (int y = 0; y <= rows; ++y)
+    {
+        float py = y * cell;
+
+        vertices.push_back(0.0f);
+        vertices.push_back(py);
+
+        vertices.push_back(width);
+        vertices.push_back(py);
+    }
 }
--- src/Render.h
+++ src/Render.h
@@ -1,12 +1,45 @@
 #ifndef RENDER_H
 #define RENDER_H
 
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <memory>
+#include <vector>
+
+#include "Shader.h"
+
 class Render
 {
 public:
     Render();
     
     void Draw();
+
+private:
+    void bindGrid();
+    void drawGrid();
+    void initGrid();
+
+private:
+    std::vector<float> vertices;
+
+    const int cols = 20;
+    const int rows = 20;
+    const float cell = 32.0f;
+
+    const float width  = cols * cell;
+    const float height = rows * cell;
+
+    glm::mat4 proj = glm::ortho(
+    0.0f,
+    width,
+    height,
+    0.0f);
+
+    std::unique_ptr<Shader> gridShaderProgram;
+
+    GLuint VAO;
 };
 
 #endif //RENDER_H
